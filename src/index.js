67a21e6c0636760f45e66f90c3a5a8d6af5b2a03
Mohamad Mahdi Tasha <imwhdiiii@gmail.com>
2023-03-04 16:58:26 +0330

    added react-router to index.js file

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 // Importing React And React Dom
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 // Creating Root With Element With Id Of 'root' Then Rendering Whole Routes In React Strict Mode
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <React.StrictMode></React.StrictMode>
+    <React.StrictMode>
+        <BrowserRouter>
+            <Routes>
+                {/*<Route path='/' element={<HomePage />}/>*/}
+            </Routes>
+        </BrowserRouter>
+    </React.StrictMode>
 );
