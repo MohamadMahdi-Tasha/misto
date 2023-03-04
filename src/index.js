4ef95701d60a933bf62a2c52388c943015708cc1
Mohamad Mahdi Tasha <imwhdiiii@gmail.com>
2023-03-04 16:48:01 +0330

    added lines of comment

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
+// Importing React And React Dom
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
+// Creating Root With Element With Id Of 'Root' Then Rendering Whole Routes In React Strict Mode
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode></React.StrictMode>
 );
