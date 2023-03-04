3ba2387d4ad9659fed4eb5eb1d9660733a31f562
Mohamad Mahdi Tasha <imwhdiiii@gmail.com>
2023-03-04 17:47:38 +0330

    added forgotten part of ui in header

--- src/index.js
+++ src/index.js
@@ -4,11 +4,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from './pages/home';
-import Header from './components/header';
+import HeaderComponent from './components/header';
 
 // Creating Root With Element With Id Of 'root' Then Rendering Whole Routes In React Strict Mode
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
+        <HeaderComponent />
         <BrowserRouter>
             <Routes>
                 <Route path='/' element={<HomePage />}/>
