a482e845137c1f91f556e9605a84f407e1ddab2b
Mohamad Mahdi Tasha <imwhdiiii@gmail.com>
2023-03-04 18:12:36 +0330

    added reset file of css and scss

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from './pages/home';
 import HeaderComponent from './components/header/header';
+import './dist/style.css';
 
 // Creating Root With Element With Id Of 'root' Then Rendering Whole Routes In React Strict Mode
 ReactDOM.createRoot(document.getElementById('root')).render(
