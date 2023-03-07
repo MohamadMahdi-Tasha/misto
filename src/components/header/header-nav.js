8ebe1fa047a4d79cb9b35eaccf1574f9565ecab4
Mohamad Mahdi Tasha <imwhdiiii@gmail.com>
2023-03-07 18:43:38 +0330

    changed hover and some other style in nav-menu

--- src/components/header/header-nav.js
+++ src/components/header/header-nav.js
@@ -27,10 +27,10 @@ export default function HeaderNavComponent (props) {
                 <img className='header__nav-img' src={logoTypo} alt="Misto Logo"/>
                 <ul className='header__nav-list'>
                     <li className='header__nav-list-item'><a className='header__nav-list-item-link' href="#">About Us</a></li>
-                    <li className='header__nav-list-item'><a className='header__nav-list-item-link header__nav-list-item-link--menu' href="#">Women</a><HeaderNavMenuComponent /></li>
-                    <li className='header__nav-list-item'><a className='header__nav-list-item-link header__nav-list-item-link--menu' href="#">Men</a><HeaderNavMenuComponent /></li>
-                    <li className='header__nav-list-item'><a className='header__nav-list-item-link header__nav-list-item-link--menu' href="#">Beauty</a><HeaderNavMenuComponent /></li>
-                    <li className='header__nav-list-item'><a className='header__nav-list-item-link header__nav-list-item-link--menu' href="#">Accessories</a><HeaderNavMenuComponent /></li>
+                    <li className='header__nav-list-item header__nav-list-item--menu-toggler'><a className='header__nav-list-item-link' href="#">Women</a><HeaderNavMenuComponent /></li>
+                    <li className='header__nav-list-item header__nav-list-item--menu-toggler'><a className='header__nav-list-item-link' href="#">Men</a><HeaderNavMenuComponent /></li>
+                    <li className='header__nav-list-item header__nav-list-item--menu-toggler'><a className='header__nav-list-item-link' href="#">Beauty</a><HeaderNavMenuComponent /></li>
+                    <li className='header__nav-list-item header__nav-list-item--menu-toggler'><a className='header__nav-list-item-link' href="#">Accessories</a><HeaderNavMenuComponent /></li>
                     <li className='header__nav-list-item'><a className='header__nav-list-item-link' href="#">Blog</a></li>
                     <li className='header__nav-list-item'><a className='header__nav-list-item-link' href="#">Contact</a></li>
                 </ul>
