f8865c07dbc18d8bd7ff7b6a9ed99a6e7f740bf5
Mohamad Mahdi Tasha <imwhdiiii@gmail.com>
2023-03-07 16:33:19 +0330

    fixed a11y problem in nav menu

--- src/components/header/header-nav-menu.js
+++ src/components/header/header-nav-menu.js
@@ -16,108 +16,108 @@ export default function HeaderNavMenuComponent() {
             <div className='header__nav-menu-inner'>
                 <ul className='header__nav-menu-list'>
                     <li className='header__nav-menu-title'>TOPS</li>
-                    <li><a className='header__nav-menu-link' href="#">Awesome</a></li>
-                    <li><a className='header__nav-menu-link' href="#">Beachwear</a></li>
-                    <li><a className='header__nav-menu-link' href="#">Beige</a></li>
-                    <li><a className='header__nav-menu-link' href="#">Cool New</a></li>
-                    <li><a className='header__nav-menu-link' href="#">Dress</a></li>
-                    <li><a className='header__nav-menu-link' href="#">Gap</a></li>
-                    <li><a className='header__nav-menu-link' href="#">Guess</a></li>
+                    <li><a tabIndex='-1' className='header__nav-menu-link' href="#">Awesome</a></li>
+                    <li><a tabIndex='-1' className='header__nav-menu-link' href="#">Beachwear</a></li>
+                    <li><a tabIndex='-1' className='header__nav-menu-link' href="#">Beige</a></li>
+                    <li><a tabIndex='-1' className='header__nav-menu-link' href="#">Cool New</a></li>
+                    <li><a tabIndex='-1' className='header__nav-menu-link' href="#">Dress</a></li>
+                    <li><a tabIndex='-1' className='header__nav-menu-link' href="#">Gap</a></li>
+                    <li><a tabIndex='-1' className='header__nav-menu-link' href="#">Guess</a></li>
                 </ul>
                 <ul className='header__nav-menu-list'>
                     <li className='header__nav-menu-title'>BOTTOMS</li>
-                    <li><a className='header__nav-menu-link' href="#">Jeans</a></li>
-                    <li><a className='header__nav-menu-link' href="#">Lacoste</a></li>
-                    <li><a className='header__nav-menu-link' href="#">Levi's</a></li>
-                    <li><a className='header__nav-menu-link' href="#">Model</a></li>
-                    <li><a className='header__nav-menu-link' href="#">Nice Featured</a></li>
-                    <li><a className='header__nav-menu-link' href="#">Polo</a></li>
-                    <li><a className='header__nav-menu-link' href="#">Pullover</a></li>
+                    <li><a tabIndex='-1' className='header__nav-menu-link' href="#">Jeans</a></li>
+                    <li><a tabIndex='-1' className='header__nav-menu-link' href="#">Lacoste</a></li>
+                    <li><a tabIndex='-1' className='header__nav-menu-link' href="#">Levi's</a></li>
+                    <li><a tabIndex='-1' className='header__nav-menu-link' href="#">Model</a></li>
+                    <li><a tabIndex='-1' className='header__nav-menu-link' href="#">Nice Featured</a></li>
+                    <li><a tabIndex='-1' className='header__nav-menu-link' href="#">Polo</a></li>
+                    <li><a tabIndex='-1' className='header__nav-menu-link' href="#">Pullover</a></li>
                 </ul>
                 <ul className='header__nav-menu-list'>
                     <li className='header__nav-menu-title'>ACCESSORIES</li>
-                    <li><a href="#" className='header__nav-menu-link'>Scarf Sale 13%</a></li>
-                    <li><a href="#" className='header__nav-menu-link'>Shirt</a></li>
-                    <li><a href="#" className='header__nav-menu-link'>Shoes</a></li>
-                    <li><a href="#" className='header__nav-menu-link'>Shorts</a></li>
-                    <li><a href="#" className='header__nav-menu-link'>Summer</a></li>
-                    <li><a href="#" className='header__nav-menu-link'>Sunglasses</a></li>
-                    <li><a href="#" className='header__nav-menu-link'>Vintage</a></li>
+                    <li><a tabIndex='-1' href="#" className='header__nav-menu-link'>Scarf Sale 13%</a></li>
+                    <li><a tabIndex='-1' href="#" className='header__nav-menu-link'>Shirt</a></li>
+                    <li><a tabIndex='-1' href="#" className='header__nav-menu-link'>Shoes</a></li>
+                    <li><a tabIndex='-1' href="#" className='header__nav-menu-link'>Shorts</a></li>
+                    <li><a tabIndex='-1' href="#" className='header__nav-menu-link'>Summer</a></li>
+                    <li><a tabIndex='-1' href="#" className='header__nav-menu-link'>Sunglasses</a></li>
+                    <li><a tabIndex='-1' href="#" className='header__nav-menu-link'>Vintage</a></li>
                 </ul>
                 <div className='header__nav-menu-slider-holder'>
                     <div className='header__nav-menu-slider-top'>
                         <h6 className='header__nav-menu-title'>BESTSELLERS</h6>
                         <div className='header__nav-menu-slider-top-btns-holder'>
-                            <button onClick={() => navMenuSlider.slideNext()} className='header__nav-menu-slider-top-btn'><svg width="6" height="10" viewBox="0 0 6 10" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M4.6498 8.8499L0.799805 4.9999L4.6498 1.1499" stroke="#121212" stroke-linecap="round" stroke-linejoin="round"/></svg></button>
-                            <button onClick={() => navMenuSlider.slidePrev()} className='header__nav-menu-slider-top-btn'><svg width="6" height="10" viewBox="0 0 6 10" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M1.34985 1.1499L5.19985 4.9999L1.34985 8.8499" stroke="#121212" stroke-linecap="round" stroke-linejoin="round"/></svg></button>
+                            <button tabIndex='-1' onClick={() => navMenuSlider.slideNext()} className='header__nav-menu-slider-top-btn'><svg width="6" height="10" viewBox="0 0 6 10" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M4.6498 8.8499L0.799805 4.9999L4.6498 1.1499" stroke="#121212" stroke-linecap="round" stroke-linejoin="round"/></svg></button>
+                            <button tabIndex='-1' onClick={() => navMenuSlider.slidePrev()} className='header__nav-menu-slider-top-btn'><svg width="6" height="10" viewBox="0 0 6 10" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M1.34985 1.1499L5.19985 4.9999L1.34985 8.8499" stroke="#121212" stroke-linecap="round" stroke-linejoin="round"/></svg></button>
                         </div>
                     </div>
                     <Swiper spaceBetween={50} slidesPerView={3} className="header__nav-menu-swiper">
                         <SwiperSlide>
-                            <a href="#">
+                            <a tabIndex='-1' href="#">
                                 <img className='header__nav-menu-slide-img' src={sliderImage} alt="Women's tracksuit Q109"/>
                                 <h6 className='header__nav-menu-slide-title'>Women's tracksuit Q109</h6>
                                 <h6 className='header__nav-menu-slide-price'>$28.99</h6>
                             </a>
                         </SwiperSlide>
                         <SwiperSlide>
-                            <a href="#">
+                            <a tabIndex='-1' href="#">
                                 <img className='header__nav-menu-slide-img' src={sliderImage} alt="Women's tracksuit Q109"/>
                                 <h6 className='header__nav-menu-slide-title'>Women's tracksuit Q109</h6>
                                 <h6 className='header__nav-menu-slide-price'>$28.99</h6>
                             </a>
                         </SwiperSlide>
                         <SwiperSlide>
-                            <a href="#">
+                            <a tabIndex='-1' href="#">
                                 <img className='header__nav-menu-slide-img' src={sliderImage} alt="Women's tracksuit Q109"/>
                                 <h6 className='header__nav-menu-slide-title'>Women's tracksuit Q109</h6>
                                 <h6 className='header__nav-menu-slide-price'>$28.99</h6>
                             </a>
                         </SwiperSlide>
                         <SwiperSlide>
-                            <a href="#">
+                            <a tabIndex='-1' href="#">
                                 <img className='header__nav-menu-slide-img' src={sliderImage} alt="Women's tracksuit Q109"/>
                                 <h6 className='header__nav-menu-slide-title'>Women's tracksuit Q109</h6>
                                 <h6 className='header__nav-menu-slide-price'>$28.99</h6>
                             </a>
                         </SwiperSlide>
                         <SwiperSlide>
-                            <a href="#">
+                            <a tabIndex='-1' href="#">
                                 <img className='header__nav-menu-slide-img' src={sliderImage} alt="Women's tracksuit Q109"/>
                                 <h6 className='header__nav-menu-slide-title'>Women's tracksuit Q109</h6>
                                 <h6 className='header__nav-menu-slide-price'>$28.99</h6>
                             </a>
                         </SwiperSlide>
                         <SwiperSlide>
-                            <a href="#">
+                            <a tabIndex='-1' href="#">
                                 <img className='header__nav-menu-slide-img' src={sliderImage} alt="Women's tracksuit Q109"/>
                                 <h6 className='header__nav-menu-slide-title'>Women's tracksuit Q109</h6>
                                 <h6 className='header__nav-menu-slide-price'>$28.99</h6>
                             </a>
                         </SwiperSlide>
                         <SwiperSlide>
-                            <a href="#">
+                            <a tabIndex='-1' href="#">
                                 <img className='header__nav-menu-slide-img' src={sliderImage} alt="Women's tracksuit Q109"/>
                                 <h6 className='header__nav-menu-slide-title'>Women's tracksuit Q109</h6>
                                 <h6 className='header__nav-menu-slide-price'>$28.99</h6>
                             </a>
                         </SwiperSlide>
                         <SwiperSlide>
-                            <a href="#">
+                            <a tabIndex='-1' href="#">
                                 <img className='header__nav-menu-slide-img' src={sliderImage} alt="Women's tracksuit Q109"/>
                                 <h6 className='header__nav-menu-slide-title'>Women's tracksuit Q109</h6>
                                 <h6 className='header__nav-menu-slide-price'>$28.99</h6>
                             </a>
                         </SwiperSlide>
                         <SwiperSlide>
-                            <a href="#">
+                            <a tabIndex='-1' href="#">
                                 <img className='header__nav-menu-slide-img' src={sliderImage} alt="Women's tracksuit Q109"/>
                                 <h6 className='header__nav-menu-slide-title'>Women's tracksuit Q109</h6>
                                 <h6 className='header__nav-menu-slide-price'>$28.99</h6>
                             </a>
                         </SwiperSlide>
                         <SwiperSlide>
-                            <a href="#">
+                            <a tabIndex='-1' href="#">
                                 <img className='header__nav-menu-slide-img' src={sliderImage} alt="Women's tracksuit Q109"/>
                                 <h6 className='header__nav-menu-slide-title'>Women's tracksuit Q109</h6>
                                 <h6 className='header__nav-menu-slide-price'>$28.99</h6>
