902a76aa8e689b0d158c10983fb51e089130ca35
Mohamad Mahdi Tasha <imwhdiiii@gmail.com>
2023-03-08 16:51:19 +0330

    added comments

--- src/components/pages/page.js
+++ src/components/pages/page.js
@@ -3,13 +3,16 @@
 import React, { useEffect } from "react";
 
 // Exporting Page Functional Component As Default
-export default function PageComponent({children}) {
+export default function PageComponent({children, pageName}) {
+    // Referencing To Page Holder
     const pageElementRef = React.createRef();
+
+    // When Page Is Mounted Add Class Of 'page--opened' To Referenced Element Above
     useEffect(() => pageElementRef.current.classList.add('page--opened'));
 
     // Returning JSX
     return (
-        <div className='page page--home' ref={pageElementRef}>
+        <div className={`page page--${pageName}`} ref={pageElementRef}>
             <div className='page__inner'>
                 {children}
             </div>
