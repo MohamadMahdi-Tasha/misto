f82a2a4a39e13dc143c68cea085ba0c642d01ce0
Mohamad Mahdi Tasha <imwhdiiii@gmail.com>
2023-03-09 10:16:34 +0330

    added bg to hover nav menus

--- src/components/header/header-nav-menu.js
+++ src/components/header/header-nav-menu.js
@@ -13,60 +13,63 @@ export default function HeaderNavMenuComponent() {
 
     // Returning JSX
     return (
-        <div className='header__nav-menu'>
-            <div className='header__nav-menu-inner'>
-                <ul className='header__nav-menu-list'>
-                    <li className='header__nav-menu-title'>TOPS</li>
-                    <li><a tabIndex='-1' className='header__nav-menu-link' href="#">Awesome</a></li>
-                    <li><a tabIndex='-1' className='header__nav-menu-link' href="#">Beachwear</a></li>
-                    <li><a tabIndex='-1' className='header__nav-menu-link' href="#">Beige</a></li>
-                    <li><a tabIndex='-1' className='header__nav-menu-link' href="#">Cool New</a></li>
-                    <li><a tabIndex='-1' className='header__nav-menu-link' href="#">Dress</a></li>
-                    <li><a tabIndex='-1' className='header__nav-menu-link' href="#">Gap</a></li>
-                    <li><a tabIndex='-1' className='header__nav-menu-link' href="#">Guess</a></li>
-                </ul>
-                <ul className='header__nav-menu-list'>
-                    <li className='header__nav-menu-title'>BOTTOMS</li>
-                    <li><a tabIndex='-1' className='header__nav-menu-link' href="#">Jeans</a></li>
-                    <li><a tabIndex='-1' className='header__nav-menu-link' href="#">Lacoste</a></li>
-                    <li><a tabIndex='-1' className='header__nav-menu-link' href="#">Levi's</a></li>
-                    <li><a tabIndex='-1' className='header__nav-menu-link' href="#">Model</a></li>
-                    <li><a tabIndex='-1' className='header__nav-menu-link' href="#">Nice Featured</a></li>
-                    <li><a tabIndex='-1' className='header__nav-menu-link' href="#">Polo</a></li>
-                    <li><a tabIndex='-1' className='header__nav-menu-link' href="#">Pullover</a></li>
-                </ul>
-                <ul className='header__nav-menu-list'>
-                    <li className='header__nav-menu-title'>ACCESSORIES</li>
-                    <li><a tabIndex='-1' href="#" className='header__nav-menu-link'>Scarf Sale 13%</a></li>
-                    <li><a tabIndex='-1' href="#" className='header__nav-menu-link'>Shirt</a></li>
-                    <li><a tabIndex='-1' href="#" className='header__nav-menu-link'>Shoes</a></li>
-                    <li><a tabIndex='-1' href="#" className='header__nav-menu-link'>Shorts</a></li>
-                    <li><a tabIndex='-1' href="#" className='header__nav-menu-link'>Summer</a></li>
-                    <li><a tabIndex='-1' href="#" className='header__nav-menu-link'>Sunglasses</a></li>
-                    <li><a tabIndex='-1' href="#" className='header__nav-menu-link'>Vintage</a></li>
-                </ul>
-                <div className='header__nav-menu-slider-holder'>
-                    <div className='header__nav-menu-slider-top'>
-                        <h6 className='header__nav-menu-title'>BESTSELLERS</h6>
-                        <div className='header__nav-menu-slider-top-btns-holder'>
-                            <button tabIndex='-1' onClick={() => navMenuSlider.slideNext()} className='header__nav-menu-slider-top-btn'><svg width="6" height="10" viewBox="0 0 6 10" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M4.6498 8.8499L0.799805 4.9999L4.6498 1.1499" stroke="#121212" strokeLinecap="round" strokeLinejoin="round"/></svg></button>
-                            <button tabIndex='-1' onClick={() => navMenuSlider.slidePrev()} className='header__nav-menu-slider-top-btn'><svg width="6" height="10" viewBox="0 0 6 10" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M1.34985 1.1499L5.19985 4.9999L1.34985 8.8499" stroke="#121212" strokeLinecap="round" strokeLinejoin="round"/></svg></button>
+        <>
+            <div className="header__nav-menu-bg"></div>
+            <div className='header__nav-menu'>
+                <div className='header__nav-menu-inner'>
+                    <ul className='header__nav-menu-list'>
+                        <li className='header__nav-menu-title'>TOPS</li>
+                        <li><a tabIndex='-1' className='header__nav-menu-link' href="#">Awesome</a></li>
+                        <li><a tabIndex='-1' className='header__nav-menu-link' href="#">Beachwear</a></li>
+                        <li><a tabIndex='-1' className='header__nav-menu-link' href="#">Beige</a></li>
+                        <li><a tabIndex='-1' className='header__nav-menu-link' href="#">Cool New</a></li>
+                        <li><a tabIndex='-1' className='header__nav-menu-link' href="#">Dress</a></li>
+                        <li><a tabIndex='-1' className='header__nav-menu-link' href="#">Gap</a></li>
+                        <li><a tabIndex='-1' className='header__nav-menu-link' href="#">Guess</a></li>
+                    </ul>
+                    <ul className='header__nav-menu-list'>
+                        <li className='header__nav-menu-title'>BOTTOMS</li>
+                        <li><a tabIndex='-1' className='header__nav-menu-link' href="#">Jeans</a></li>
+                        <li><a tabIndex='-1' className='header__nav-menu-link' href="#">Lacoste</a></li>
+                        <li><a tabIndex='-1' className='header__nav-menu-link' href="#">Levi's</a></li>
+                        <li><a tabIndex='-1' className='header__nav-menu-link' href="#">Model</a></li>
+                        <li><a tabIndex='-1' className='header__nav-menu-link' href="#">Nice Featured</a></li>
+                        <li><a tabIndex='-1' className='header__nav-menu-link' href="#">Polo</a></li>
+                        <li><a tabIndex='-1' className='header__nav-menu-link' href="#">Pullover</a></li>
+                    </ul>
+                    <ul className='header__nav-menu-list'>
+                        <li className='header__nav-menu-title'>ACCESSORIES</li>
+                        <li><a tabIndex='-1' href="#" className='header__nav-menu-link'>Scarf Sale 13%</a></li>
+                        <li><a tabIndex='-1' href="#" className='header__nav-menu-link'>Shirt</a></li>
+                        <li><a tabIndex='-1' href="#" className='header__nav-menu-link'>Shoes</a></li>
+                        <li><a tabIndex='-1' href="#" className='header__nav-menu-link'>Shorts</a></li>
+                        <li><a tabIndex='-1' href="#" className='header__nav-menu-link'>Summer</a></li>
+                        <li><a tabIndex='-1' href="#" className='header__nav-menu-link'>Sunglasses</a></li>
+                        <li><a tabIndex='-1' href="#" className='header__nav-menu-link'>Vintage</a></li>
+                    </ul>
+                    <div className='header__nav-menu-slider-holder'>
+                        <div className='header__nav-menu-slider-top'>
+                            <h6 className='header__nav-menu-title'>BESTSELLERS</h6>
+                            <div className='header__nav-menu-slider-top-btns-holder'>
+                                <button tabIndex='-1' onClick={() => navMenuSlider.slideNext()} className='header__nav-menu-slider-top-btn'><svg width="6" height="10" viewBox="0 0 6 10" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M4.6498 8.8499L0.799805 4.9999L4.6498 1.1499" stroke="#121212" strokeLinecap="round" strokeLinejoin="round"/></svg></button>
+                                <button tabIndex='-1' onClick={() => navMenuSlider.slidePrev()} className='header__nav-menu-slider-top-btn'><svg width="6" height="10" viewBox="0 0 6 10" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M1.34985 1.1499L5.19985 4.9999L1.34985 8.8499" stroke="#121212" strokeLinecap="round" strokeLinejoin="round"/></svg></button>
+                            </div>
                         </div>
+                        <Swiper spaceBetween={50} slidesPerView={3} className="header__nav-menu-swiper">
+                            <SwiperSlide><HeaderNavMenuSwiperSlideComponent img={sliderImage} name="Women's tracksuit Q109" price={2899}/></SwiperSlide>
+                            <SwiperSlide><HeaderNavMenuSwiperSlideComponent img={sliderImage} name="Women's tracksuit Q109" price={2899}/></SwiperSlide>
+                            <SwiperSlide><HeaderNavMenuSwiperSlideComponent img={sliderImage} name="Women's tracksuit Q109" price={2899}/></SwiperSlide>
+                            <SwiperSlide><HeaderNavMenuSwiperSlideComponent img={sliderImage} name="Women's tracksuit Q109" price={2899}/></SwiperSlide>
+                            <SwiperSlide><HeaderNavMenuSwiperSlideComponent img={sliderImage} name="Women's tracksuit Q109" price={2899}/></SwiperSlide>
+                            <SwiperSlide><HeaderNavMenuSwiperSlideComponent img={sliderImage} name="Women's tracksuit Q109" price={2899}/></SwiperSlide>
+                            <SwiperSlide><HeaderNavMenuSwiperSlideComponent img={sliderImage} name="Women's tracksuit Q109" price={2899}/></SwiperSlide>
+                            <SwiperSlide><HeaderNavMenuSwiperSlideComponent img={sliderImage} name="Women's tracksuit Q109" price={2899}/></SwiperSlide>
+                            <SwiperSlide><HeaderNavMenuSwiperSlideComponent img={sliderImage} name="Women's tracksuit Q109" price={2899}/></SwiperSlide>
+                            <SwiperSlide><HeaderNavMenuSwiperSlideComponent img={sliderImage} name="Women's tracksuit Q109" price={2899}/></SwiperSlide>
+                        </Swiper>
                     </div>
-                    <Swiper spaceBetween={50} slidesPerView={3} className="header__nav-menu-swiper">
-                        <SwiperSlide><HeaderNavMenuSwiperSlideComponent img={sliderImage} name="Women's tracksuit Q109" price={2899}/></SwiperSlide>
-                        <SwiperSlide><HeaderNavMenuSwiperSlideComponent img={sliderImage} name="Women's tracksuit Q109" price={2899}/></SwiperSlide>
-                        <SwiperSlide><HeaderNavMenuSwiperSlideComponent img={sliderImage} name="Women's tracksuit Q109" price={2899}/></SwiperSlide>
-                        <SwiperSlide><HeaderNavMenuSwiperSlideComponent img={sliderImage} name="Women's tracksuit Q109" price={2899}/></SwiperSlide>
-                        <SwiperSlide><HeaderNavMenuSwiperSlideComponent img={sliderImage} name="Women's tracksuit Q109" price={2899}/></SwiperSlide>
-                        <SwiperSlide><HeaderNavMenuSwiperSlideComponent img={sliderImage} name="Women's tracksuit Q109" price={2899}/></SwiperSlide>
-                        <SwiperSlide><HeaderNavMenuSwiperSlideComponent img={sliderImage} name="Women's tracksuit Q109" price={2899}/></SwiperSlide>
-                        <SwiperSlide><HeaderNavMenuSwiperSlideComponent img={sliderImage} name="Women's tracksuit Q109" price={2899}/></SwiperSlide>
-                        <SwiperSlide><HeaderNavMenuSwiperSlideComponent img={sliderImage} name="Women's tracksuit Q109" price={2899}/></SwiperSlide>
-                        <SwiperSlide><HeaderNavMenuSwiperSlideComponent img={sliderImage} name="Women's tracksuit Q109" price={2899}/></SwiperSlide>
-                    </Swiper>
                 </div>
             </div>
-        </div>
+        </>
     );
 }
