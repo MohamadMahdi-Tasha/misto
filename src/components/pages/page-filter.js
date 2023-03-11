7cd7e16a80ed9f54389e953101b96a7a42d563cd
Mohamad Mahdi Tasha <imwhdiiii@gmail.com>
2023-03-11 12:18:16 +0330

    fixed bugs in filter buttons

--- src/components/pages/page-filter.js
+++ src/components/pages/page-filter.js
@@ -1,11 +1,22 @@
 // Codes By Mahdi Tasha
 // Importing Part
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 
 // Exporting Page Filter Functional Component As Default
 export default function PageFilterComponent({title, selectedItemIndex, btnItems}) {
-    // Creating State For Index Of Selected Dropdown
-    const [numberOfSelectedDropDown, setNumberOfSelectedDropDown] =  useState(selectedItemIndex);
+    // Initializing States
+    const [isDropDownOpened, setOpenOrCloseOfDropDown] = useState('false');
+    const [dropDownTogglerClicks, setDropDownTogglerClicks] = useState(0);
+    const [selectedIndex, setSelectedIndex] = useState(selectedItemIndex);
+
+    // Handling Click Of Mobile DropDown Toggler
+    function handleClickOfMobileDropDownToggler() {
+        setDropDownTogglerClicks(prevState => prevState + 1);
+
+        (dropDownTogglerClicks % 2 !== 0)
+            ? setOpenOrCloseOfDropDown('true')
+            : setOpenOrCloseOfDropDown('false')
+    }
 
     // Creating Refrence To Buttos Holder
     const refrenceOfButtonsHolder = React.createRef();
@@ -15,14 +26,14 @@ export default function PageFilterComponent({title, selectedItemIndex, btnItems}
         <div className='page--home__filter-btns-holder'>
             <h1 className='page--home__filter-btns-title'>{title}</h1>
             <div className='page--home__filter-btns-list-holder'>
-                <button className='page--home__filter-mobile-dropdown-toggler'>
+                <button data-opened={isDropDownOpened} onClick={handleClickOfMobileDropDownToggler} className='page--home__filter-mobile-dropdown-toggler'>
                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
                          className="bi bi-three-dots-vertical" viewBox="0 0 16 16">
                         <path
                             d="M9.5 13a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0z"/>
                     </svg>
                 </button>
-                <ul className='page--home__filter-btns-list' ref={refrenceOfButtonsHolder}>
+                <ul data-opened={isDropDownOpened} className='page--home__filter-btns-list' ref={refrenceOfButtonsHolder}>
                     {
                         btnItems.map((item) =>
                             <li key={btnItems.indexOf(item)}>
@@ -35,9 +46,9 @@ export default function PageFilterComponent({title, selectedItemIndex, btnItems}
                                         const selectedFilterBtn = currentElementOfRef.querySelector('.page--home__filter-btn[data-selected="true"]');
                                         const buttonToSelect = currentElementOfRef.children[IndexOfBtn].firstElementChild;
 
-                                        setNumberOfSelectedDropDown(IndexOfBtn);
                                         selectedFilterBtn.setAttribute('data-selected', 'false');
                                         buttonToSelect.setAttribute('data-selected', 'true');
+                                        setSelectedIndex(IndexOfBtn);
                                     }}
                                 >{item}</button>
                             </li>
