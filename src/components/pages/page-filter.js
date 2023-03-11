d2b185a1bed141ec586ddf2825380fb0d657f8b7
Mohamad Mahdi Tasha <imwhdiiii@gmail.com>
2023-03-11 11:54:59 +0330

    added some structure in filter-btns component

--- src/components/pages/page-filter.js
+++ src/components/pages/page-filter.js
@@ -14,28 +14,37 @@ export default function PageFilterComponent({title, selectedItemIndex, btnItems}
     return (
         <div className='page--home__filter-btns-holder'>
             <h1 className='page--home__filter-btns-title'>{title}</h1>
-            <ul className='page--home__filter-btns-list' ref={refrenceOfButtonsHolder}>
-                {
-                    btnItems.map((item) =>
-                        <li key={btnItems.indexOf(item)}>
-                            <button
-                                data-selected={(selectedItemIndex === btnItems.indexOf(item)) ? 'true' : 'false'}
-                                className='page--home__filter-btn'
-                                onClick={() => {
-                                    const IndexOfBtn = btnItems.indexOf(item);
-                                    const currentElementOfRef = refrenceOfButtonsHolder.current;
-                                    const selectedFilterBtn = currentElementOfRef.querySelector('.page--home__filter-btn[data-selected="true"]');
-                                    const buttonToSelect = currentElementOfRef.children[IndexOfBtn].firstElementChild;
+            <div className='page--home__filter-btns-list-holder'>
+                <button className='page--home__filter-mobile-dropdown-toggler'>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                         className="bi bi-three-dots-vertical" viewBox="0 0 16 16">
+                        <path
+                            d="M9.5 13a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0z"/>
+                    </svg>
+                </button>
+                <ul className='page--home__filter-btns-list' ref={refrenceOfButtonsHolder}>
+                    {
+                        btnItems.map((item) =>
+                            <li key={btnItems.indexOf(item)}>
+                                <button
+                                    data-selected={(selectedItemIndex === btnItems.indexOf(item)) ? 'true' : 'false'}
+                                    className='page--home__filter-btn'
+                                    onClick={() => {
+                                        const IndexOfBtn = btnItems.indexOf(item);
+                                        const currentElementOfRef = refrenceOfButtonsHolder.current;
+                                        const selectedFilterBtn = currentElementOfRef.querySelector('.page--home__filter-btn[data-selected="true"]');
+                                        const buttonToSelect = currentElementOfRef.children[IndexOfBtn].firstElementChild;
 
-                                    setNumberOfSelectedDropDown(IndexOfBtn);
-                                    selectedFilterBtn.setAttribute('data-selected', 'false');
-                                    buttonToSelect.setAttribute('data-selected', 'true');
-                                }}
+                                        setNumberOfSelectedDropDown(IndexOfBtn);
+                                        selectedFilterBtn.setAttribute('data-selected', 'false');
+                                        buttonToSelect.setAttribute('data-selected', 'true');
+                                    }}
                                 >{item}</button>
-                        </li>
-                    )
-                }
-            </ul>
+                            </li>
+                        )
+                    }
+                </ul>
+            </div>
         </div>
     );
 }
