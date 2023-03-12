e80026d7196e03df1f6173d351c91912a46021e0
Mohamad Mahdi Tasha <imwhdiiii@gmail.com>
2023-03-12 12:21:44 +0330

    added class names of last commit

--- src/components/pages/page-product-items-loader.js
+++ src/components/pages/page-product-items-loader.js
@@ -3,12 +3,12 @@
 export default function PageProductItemsLoaderComponent() {
     // Returning JSX
     return(
-        <li>
-            <div></div>
-            <div></div>
-            <div>
-                <div></div>
-                <div></div>
+        <li className='page__products-item-loader'>
+            <div className='page__products-item-loader__img'></div>
+            <div className='page__products-item-loader__title'></div>
+            <div className='page__products-item-loader__bottom-holder'>
+                <div className='page__products-item-loader__price'></div>
+                <div className='page__products-item-loader__score'></div>
             </div>
         </li>
     );
