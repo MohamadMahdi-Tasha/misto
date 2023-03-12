8e5df0d244e6caa21c7888d3b073bece71bc172b
Mohamad Mahdi Tasha <imwhdiiii@gmail.com>
2023-03-12 14:29:05 +0330

    yea

---
+++ src/components/pages/pageCategoriesBreadCrumb.js
@@ -0,0 +1,12 @@
+// Codes By Mahdi Tasha
+// Exporting Page Categories BreadCrumb Component As Functional Component
+export default function PageCategoriesBreadCrumbComponent() {
+    // Returning JSX
+    return (
+        <div>
+            <h6>Home</h6>
+            <span>></span>
+            <h6>Women</h6>
+        </div>
+    );
+}
