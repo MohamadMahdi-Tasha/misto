007807b9e648d14833722d09f20ec8d7df2989f6
Mohamad Mahdi Tasha <imwhdiiii@gmail.com>
2023-03-12 14:51:38 +0330

    yea idk

--- src/components/pages/pageCategoriesBreadCrumb.js
+++ src/components/pages/pageCategoriesBreadCrumb.js
@@ -1,12 +1,34 @@
 // Codes By Mahdi Tasha
+// Importing Parts
+import { useLocation } from "react-router-dom";
+
 // Exporting Page Categories BreadCrumb Component As Functional Component
 export default function PageCategoriesBreadCrumbComponent() {
+    // Using 'useLocation' Hook To Find Path Of Page
+    const url = useLocation();
+    const pathNameOfUrl = url.pathname;
+    const pathNameWithoutFistSlash = pathNameOfUrl.slice(1, pathNameOfUrl.length);
+    const arrayOfPathes = pathNameWithoutFistSlash.split('/');
+
+    // A Little Component For Name And Icon Of BreaCrumbs
+    function BreadCrumbInnerNameComponent({name}) {
+        // Returning JSX
+        return (
+            <>
+                {
+                    (name === undefined)
+                        ? <h6>Home</h6>
+                        : <h6>{name}</h6>
+                }
+            </>
+        );
+    }
+
     // Returning JSX
     return (
         <div>
-            <h6>Home</h6>
-            <span>></span>
-            <h6>Women</h6>
+            <BreadCrumbInnerNameComponent/>
+            {arrayOfPathes.map(item => <BreadCrumbInnerNameComponent name={item}/>)}
         </div>
     );
 }
